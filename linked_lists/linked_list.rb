e5bbc1c07e94fd6ae43a350d772169692b796cc6
Rebecca Qu <rebecca@lookbookhq.com>
2017-09-21 12:48:35 -0400

    [Linked Lists] remove unnecessary default values

--- linked_lists/linked_list.rb
+++ linked_lists/linked_list.rb
@@ -1,9 +1,9 @@
 class Node
   attr_accessor :value, :next_node
 
-  def initialize(value = nil, next_node = nil)
+  def initialize(value)
     @value = value
-    @next_node = next_node
+    @next_node = nil
   end
 end
 
